f7c6ab909d1a492837734200009e6cab12c4841c
Anton <a.yakubovych@yahoo.com>
2020-07-04 08:29:49 -0400

    infoBar textes

--- src/common/mainPage/InfoBar.js
+++ src/common/mainPage/InfoBar.js
@@ -18,31 +18,29 @@ export default function InfoBar() {
             <div className={"info_bar_div"}>
                 <img src={descriptionIcon} width="45" height="45" alt="disc" style={{marginLeft:'-6px'}}/>
                 <h5 className={"home_info_top_text"}>Project description</h5>
-                <h6 className={"home_info_mid_text"}>More than 8000 lines of code in 3 services,deployed on hand made and self configure server</h6>
+                <h6 className={"home_info_mid_text"}>More than 8000 lines of code in 3 services, deployed on handmade and self-configure server.</h6>
                 <span onClick={() => aboutHref()} className={"home_info_link"}>About ></span>
             </div>
 
             <div className={"info_bar_div"} style={{marginLeft:'300px'}}>
-                <img src={contactIcon} width="45" height="45" alt="disc" style={{marginLeft:'-3px'}}/>
-                <h5 className={"home_info_top_text"}>Contact</h5>
-                <h6 className={"home_info_mid_text"} style={{width:'270px'}}>If you want to contact me, click here and chose the best way for you to do it.</h6>
-                <span onClick={() => contactHref()} className={"home_info_link"}>Contact ></span>
+                <img src={guideIcon} width="45" height="45" alt="disc" style={{marginLeft:'-6px'}}/>
+                <h5 className={"home_info_top_text"}>Usage guide</h5>
+                <h6 className={"home_info_mid_text"}>Simple instruction of application. Learn how to use it right and fast for your better experience.</h6>
+                <span onClick={() => aboutHref()} className={"home_info_link"}>Guide ></span>
             </div>
-{/*
+
             <div className={"info_bar_div"} style={{marginLeft:'600px'}}>
                 <img src={humanIcon} width="45" height="45" alt="disc" style={{marginLeft:'-3px'}}/>
-                <h5 className={"home_info_top_text"}>Team</h5>
-                <h6 className={"home_info_mid_text"}>More than 8000 lines of code in 3 services,deployed on hand made and self configure server</h6>
-                <span onClick={() => aboutHref()} className={"home_info_link"}>About ></span>
+                <h5 className={"home_info_top_text"}>Our core team</h5>
+                <h6 className={"home_info_mid_text"}>Complete information about team members, who worked hard with pleasure, but on a cost-free basis.</h6>
+                <span onClick={() => aboutHref()} className={"home_info_link"}>Team ></span>
             </div>
 
             <div className={"info_bar_div"} style={{marginLeft:'900px'}}>
-                <img src={guideIcon} width="45" height="45" alt="disc" style={{marginLeft:'-6px'}}/>
-                <h5 className={"home_info_top_text"}>Usage guide</h5>
-                <h6 className={"home_info_mid_text"}>More than 8000 lines of code in 3 services,deployed on hand made and self configure server</h6>
-                <span onClick={() => aboutHref()} className={"home_info_link"}>About ></span>
+                <img src={contactIcon} width="45" height="45" alt="disc" style={{marginLeft:'-3px'}}/>
+                <h5 className={"home_info_top_text"}>Contact</h5>
+                <h6 className={"home_info_mid_text"}>If you want to contact me, click here and choose the most convenient way for you to do this.</h6>
+                <span onClick={() => contactHref()} className={"home_info_link"}>Contact ></span>
             </div>
-*/}
-
         </div>
     )};
